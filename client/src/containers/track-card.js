5704f815e067576e95e6e58dc7ac004e8255ef91
Luis Alberto Mendoza Hernandez <luis.mendoza@MacBookPro-LuisMendoza.local>
2023-03-23 11:15:20 -0500

    first commit

--- client/src/containers/track-card.js
+++ client/src/containers/track-card.js
@@ -3,7 +3,24 @@ import styled from '@emotion/styled';
 import { colors, mq } from '../styles';
 import { humanReadableTimeFromSeconds } from '../utils/helpers';
 import { Link } from 'react-router-dom';
+import { gql, useMutation } from '@apollo/client';
 
+/**
+ * Mutation to increment a track's number of views
+ */
+const INCREMENT_TRACK_VIEWS = gql`
+  mutation IncrementTrackViews($incrementTrackViewsId: ID!) {
+    incrementTrackViews(id: $incrementTrackViewsId) {
+      code
+      success
+      message
+      track {
+        id
+        numberOfViews
+      }
+    }
+  }
+`;
 /**
  * Track Card component renders basic info in a card format
  * for each track populating the tracks grid homepage.
@@ -11,8 +28,16 @@ import { Link } from 'react-router-dom';
 const TrackCard = ({ track }) => {
   const { title, thumbnail, author, length, modulesCount, id } = track;
 
+  const [incrementTrackViews] = useMutation(INCREMENT_TRACK_VIEWS, {
+    variables: { incrementTrackViewsId: id },
+    // to observe what the mutation response returns
+    onCompleted: (data) => {
+      console.log(data);
+    }
+  });
+
   return (
-    <CardContainer to={`/track/${id}`}>
+    <CardContainer to={`/track/${id}`} onClick={incrementTrackViews}>
       <CardContent>
         <CardImageContainer>
           <CardImage src={thumbnail} alt={title} />
@@ -48,30 +73,30 @@ const CardContainer = styled(Link)({
   flexDirection: 'column',
   justifyContent: 'space-between',
   [mq[0]]: {
-    width: '90%',
+    width: '90%'
   },
   [mq[1]]: {
-    width: '47%',
+    width: '47%'
   },
   [mq[2]]: {
-    width: '31%',
+    width: '31%'
   },
   height: 380,
   margin: 10,
   overflow: 'hidden',
   position: 'relative',
   ':hover': {
-    backgroundColor: colors.pink.lightest,
+    backgroundColor: colors.pink.lightest
   },
   cursor: 'pointer',
-  textDecoration: 'none',
+  textDecoration: 'none'
 });
 
 const CardContent = styled.div({
   display: 'flex',
   flexDirection: 'column',
   justifyContent: 'space-around',
-  height: '100%',
+  height: '100%'
 });
 
 const CardTitle = styled.h3({
@@ -80,7 +105,7 @@ const CardTitle = styled.h3({
   lineHeight: '1em',
   fontWeight: 700,
   color: colors.text,
-  flex: 1,
+  flex: 1
 });
 
 const CardImageContainer = styled.div({
@@ -93,15 +118,15 @@ const CardImageContainer = styled.div({
     bottom: 0,
     left: 0,
     right: 0,
-    background: 'rgba(250,0,150,0.20)',
-  },
+    background: 'rgba(250,0,150,0.20)'
+  }
 });
 
 const CardImage = styled.img({
   objectFit: 'cover',
   width: '100%',
   height: '100%',
-  filter: 'grayscale(60%)',
+  filter: 'grayscale(60%)'
 });
 
 const CardBody = styled.div({
@@ -110,12 +135,12 @@ const CardBody = styled.div({
   display: 'flex',
   color: colors.textSecondary,
   flexDirection: 'column',
-  justifyContent: 'space-around',
+  justifyContent: 'space-around'
 });
 
 const CardFooter = styled.div({
   display: 'flex',
-  flexDirection: 'Row',
+  flexDirection: 'Row'
 });
 
 const AuthorImage = styled.img({
@@ -123,20 +148,20 @@ const AuthorImage = styled.img({
   width: 30,
   marginRight: 8,
   borderRadius: '50%',
-  objectFit: 'cover',
+  objectFit: 'cover'
 });
 
 const AuthorAndTrack = styled.div({
   display: 'flex',
   flexDirection: 'column',
-  justifyContent: 'space-between',
+  justifyContent: 'space-between'
 });
 
 const AuthorName = styled.div({
   lineHeight: '1em',
-  fontSize: '1.1em',
+  fontSize: '1.1em'
 });
 
 const TrackLength = styled.div({
-  fontSize: '0.8em',
+  fontSize: '0.8em'
 });
--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -13,7 +13,28 @@ const resolvers = {
     // get a single module by ID, for the module detail page
     module: (_, { id }, { dataSources }) => {
       return dataSources.trackAPI.getModule(id);
-    },
+    }
+  },
+  Mutation: {
+    // increments a track's numberOfViews property
+    incrementTrackViews: async (_, { id }, { dataSources }) => {
+      try {
+        const track = await dataSources.trackAPI.incrementTrackViews(id);
+        return {
+          code: 200,
+          success: true,
+          message: `Successfully incremented number of views for track ${id}`,
+          track
+        };
+      } catch (err) {
+        return {
+          code: err.extensions.response.status,
+          success: false,
+          message: err.extensions.response.body,
+          track: null
+        };
+      }
+    }
   },
   Track: {
     author: ({ authorId }, _, { dataSources }) => {
@@ -22,8 +43,8 @@ const resolvers = {
 
     modules: ({ id }, _, { dataSources }) => {
       return dataSources.trackAPI.getTrackModules(id);
-    },
-  },
+    }
+  }
 };
 
 module.exports = resolvers;
